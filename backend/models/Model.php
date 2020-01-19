9313158940971f1c3fa31e17a747400c4b54cee7
Igor Barbosa <igor.barbosa.dev@gmail.com>
2020-01-19 19:34:50 -0300

    Inicializando conexão com banco de dados com charset UTF8

--- backend/models/Model.php
+++ backend/models/Model.php
@@ -14,7 +14,7 @@
 
 
         public function __construct(){            
-            $this->pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.'', DB_USER, DB_PASS);
+            $this->pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASS);
             $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         }
 
